9e77ea0900a83d619efdebc9231f9e449fa0471a
Orlov Kirill <orlov.kirill13@gmail.com>
2024-05-19 18:12:27 +0300

    edited

--- decorator/decorator.cpp
+++ decorator/decorator.cpp
@@ -1,78 +1,133 @@
-// Decorator design pattern
-
+/**
+ * The base Component interface defines operations that can be altered by
+ * decorators.
+ */
 #include <iostream>
-#include <memory>
-
-// interface for Window and Decorator
-struct IWindow {
-    virtual void draw() const = 0;
-    virtual ~IWindow() = default;
+class Drink {
+ public:
+  virtual ~Drink() {}
+  virtual std::string GetDescription() const = 0;
+    virtual double GetCost() const = 0;
 };
-
-// concrete Window
-class Window : public IWindow {
-  public:
-    void draw() const override { std::cout << "Basic Window\n"; }
+/**
+ * Concrete Components provide default implementations of the operations. There
+ * might be several variations of these classes.
+ */
+class Water : public Drink {
+    const double cost=1.0;
+ public:
+  std::string GetDescription() const override {
+    return "Water";
+  }
+  double GetCost() const override
+  {
+      return cost;
+  }
 };
+/**
+ * The base Decorator class follows the same interface as the other components.
+ * The primary purpose of this class is to define the wrapping interface for all
+ * concrete decorators. The default implementation of the wrapping code might
+ * include a field for storing a wrapped component and the means to initialize
+ * it.
+ */
+class DrinkDecorator : public Drink {
+  /**
+   * @var Component
+   */
+ protected:
+  Drink* component_;
 
-// fancy Window
-class FancyWindow : public Window {
-  public:
-    void draw() const override { std::cout << "Fancy Window\n"; }
+ public:
+  DrinkDecorator(Drink* component) : component_(component) {
+  }
+  /**
+   * The Decorator delegates all work to the wrapped component.
+   */
+  std::string GetDescription() const override {
+    return this->component_->GetDescription();
+  }
+  double GetCost() const override
+  {
+      return  this->component_->GetCost();
+  }
 };
+/**
+ * Concrete Decorators call the wrapped object and alter its result in some way.
+ */
+class Milk : public DrinkDecorator {
+  /**
+   * Decorators may call parent implementation of the operation, instead of
+   * calling the wrapped object directly. This approach simplifies extension of
+   * decorator classes.
+   */
+        const double cost=1.5;
+ public:
+  Milk(Drink* component) : DrinkDecorator(component) {
+  }
 
-// basic Decorator
-class Decorator : public IWindow {
-    std::unique_ptr<IWindow> _window; // has a
-  public:
-    explicit Decorator(std::unique_ptr<IWindow> window)
-        : _window{std::move(window)} {}
-    void draw() const override {
-        _window->draw(); // delegate responsibility
-    }
+  std::string GetDescription() const override {
+    return "Milk + " + DrinkDecorator::GetDescription() ;
+  }
+  double GetCost() const override
+  {
+      return  this->component_->GetCost()+cost;
+  }
 };
+/**
+ * Decorators can execute their behavior either before or after the call to a
+ * wrapped object.
+ */
+class coffe : public DrinkDecorator {
 
-// add borders
-class BorderDecorator : public Decorator {
-  public:
-    explicit BorderDecorator(std::unique_ptr<IWindow> window)
-        : Decorator(std::move(window)) {}
-    void draw() const override {
-        Decorator::draw();
-        std::cout << "\twith Border\n";
-    }
+        const double cost=2.0;
+ public:
+  coffe(Drink* component) : DrinkDecorator(component) {
+  }
+  double GetCost() const override
+  {
+      return  this->component_->GetCost()+cost;
+  }
+  std::string GetDescription() const override {
+    return "Coffe + " + DrinkDecorator::GetDescription() ;
+  }
 };
-
-// add scrollbars
-class ScrollBarDecorator : public Decorator {
-  public:
-    explicit ScrollBarDecorator(std::unique_ptr<IWindow> window)
-        : Decorator(std::move(window)) {}
-    void draw() const override {
-        Decorator::draw();
-        std::cout << "\twith ScrollBar\n";
-    }
-};
+/**
+ * The client code works with all objects using the Component interface. This
+ * way it can stay independent of the concrete classes of components it works
+ * with.
+ */
+void ClientCode(Drink* component) {
+  // ...
+  std::cout << "Description: " << component->GetDescription()<<std::endl;
+    std::cout << "total cost is:"<<component->GetCost()<<std::endl;
+  // ...
+}
 
 int main() {
-    // decorate a basic Window
-    std::unique_ptr<IWindow> decorated_window =
-        std::make_unique<BorderDecorator>(std::make_unique<BorderDecorator>(
-            std::make_unique<ScrollBarDecorator>(std::make_unique<Window>())));
-    // display it
-    decorated_window->draw();
+  /**
+   * This way the client code can support both simple components...
+   */
+  Drink* simple = new Water;
+  std::cout << "Client: I've got a simple component:\n";
+  ClientCode(simple);
+  std::cout << "\n\n";
+  /**
+   * ...as well as decorated ones.
+   *
+   * Note how decorators can wrap not only simple components but the other
+   * decorators as well.
+   */
+  Drink* decorator1 = new Milk(simple);
+  Drink* decorator2 = new coffe(decorator1);
 
-    // decorate a FancyWindow
-    std::unique_ptr<IWindow> decorated_fancy_window =
-        std::make_unique<BorderDecorator>(std::make_unique<BorderDecorator>(
-            std::make_unique<ScrollBarDecorator>(
-                std::make_unique<FancyWindow>())));
-    // display it
-    decorated_fancy_window->draw();
 
-    // a basically decorated window
-    std::unique_ptr<IWindow> basic_decorated_window =
-        std::make_unique<Decorator>(std::make_unique<Window>());
-    // display it
-    basic_decorated_window->draw();
+  ClientCode(decorator2);
+
+
+  delete simple;
+  delete decorator1;
+  delete decorator2;
+
+  return 0;
 }
---
+++ tests/tests.cpp
@@ -0,0 +1,36 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "decorator.cpp"
+#include <string>
+
+TEST(DecoratorTest, SimpleDrink) {
+  Drink* drink = new Water();
+  testing::internal::CaptureStdout();
+  ClientCode(drink);
+  std::string output = testing::internal::GetCapturedStdout();
+  EXPECT_EQ(output, "Description: Water\ntotal cost is:1\n");
+  delete drink;
+}
+
+TEST(DecoratorTest, DrinkWithMilk) {
+  Drink* drink = new Water();
+  drink = new Milk(drink);
+  testing::internal::CaptureStdout();
+  ClientCode(drink);
+  std::string output = testing::internal::GetCapturedStdout();
+  EXPECT_EQ(output, "Description: Milk + Water\ntotal cost is:2.5\n");
+  delete drink;
+}
+
+TEST(DecoratorTest, DrinkWithMilkAndCoffee) {
+  Drink* drink = new Water();
+  drink = new Milk(drink);
+  drink = new coffe(drink);
+  testing::internal::CaptureStdout();
+  ClientCode(drink);
+  std::string output = testing::internal::GetCapturedStdout();
+  EXPECT_EQ(output, "Description: Coffe + Milk + Water\ntotal cost is:4.5\n");
+  delete drink;
+}
+
+
